d9c9fa09efbf1d5d21df839b524646a063c54d2d
lcougnaud <laure.cougnaud@openanalytics.eu>
2018-12-19 12:22:17 +0100

    include possibility to include table in the summary plot (fixes #17281)

--- package/inTextSummaryTable/R/subjectProfileSummaryPlot.R
+++ package/inTextSummaryTable/R/subjectProfileSummaryPlot.R
@@ -1,7 +1,6 @@
 #' Plot subject summary profile
 #' @param data Data.frame with summary statistics to represent in the plot,
 #' e.g. produced with the \code{\link{computeSummaryStatisticsTable}}.
-#' @param xVar String, variable of \code{data} with variable for the x-axis.
 #' @param xLab String with label for the \code{xVar}.
 #' @param meanVar String, variable of \code{data} with the mean variable.
 #' @param seVar String, variable of \code{data} with the standard error.
@@ -9,14 +8,10 @@
 #' @param facetVar String, variable of \code{data} for facetting.
 #' @param facetScale String with type of scale used for facetting, 'free_y' by default
 #' (fixed scale in the x-axis and free in the y-axis).
-#' @param colorVar String, variable of \code{data} for coloring.
-#' @param colorLab String, label for \code{colorVar}, used in the legend.
-#' @param colorPalette (named) Vector with color palette.
 #' @param useLinetype Logical, if TRUE (FALSE by default) use also linetype
 #' to differenciate the variable specified via \code{colorVar} in the mean line.
 #' @param linetypePalette Vector with linetype(s), in case \code{useLinetype} is TRUE.
 #' @param title String with title for the plot.
-#' @param labelVars Named string with variable labels (names are the variable code).
 #' @param jitter Numeric with jitter for the x-axis, only used if \code{colorVar} specified.
 #' @param label Logical or expression.
 #' Points are labelled with \code{meanVar} if set to TRUE,
@@ -25,10 +20,23 @@
 #' @param sizeLine Size for the line.
 #' @param sizeLabel Size for the label, only used if \code{label} is not NULL.
 #' @param xAxisLabs (optional) Named character vector with labels for the x-axis.
-#' @return \code{\link[ggplot2]{ggplot}} object
+#' @param yLim Vector of the length 2 with limits for the y-axis.
+#' @param tableText (optional) Character vector with colname of \code{data}
+#' or expression from colnames of \code{data} to be represented in
+#' the table below the plot.
+#' By default, no table is displayed.
+#' @param tableLabel Character vector with label for \code{tableText},
+#' used as label for the x-axis of the table.
+#' @param tableHeight Numeric of length 1 with height for the table.
+#' @param byVar Variable(s) of \code{data} for which separated plot(s)
+#' should be created.
+#' @inheritParams subjectProfileSummaryTable
+#' @return \code{\link[ggplot2]{ggplot}} object or list of such
+#' objects of \code{byVar} is specified.
 #' @author Laure Cougnaud
 #' @importFrom glpgUtilityFct getGLPGColorPalette getGLPGShapePalette getLabelVar
 #' @import ggplot2
+#' @import cowplot
 #' @importFrom ggrepel geom_text_repel
 #' @export
 subjectProfileSummaryPlot <- function(data,
@@ -45,12 +53,31 @@ subjectProfileSummaryPlot <- function(data,
 	shapePalette = NULL,
 	jitter = NULL,
 	title = NULL,
-	ylim = NULL, xlim = NULL,
+	yLim = NULL, xLim = NULL,
 	xAxisLabs = NULL,
 	sizePoint = GeomPoint$default_aes$size,
 	sizeLine = GeomLine$default_aes$size,
 	sizeLabel = GeomText$default_aes$size,
-	label = FALSE){
+	tableText = NULL, tableLabel = NULL, tableHeight = 0.2,
+	label = FALSE,
+	byVar = NULL){
+
+	if(!is.null(byVar)){
+		inputParams <- as.list(environment())
+		if(!byVar %in% colnames(data)){
+			warning("'byVar' is not available in the 'data' so is not used.")
+			byVar <- FALSE
+		}else{
+			res <- dlply(data, byVar, function(dataBy){
+				inputParamsBy <- inputParams
+				inputParamsBy$data <- dataBy
+				inputParamsBy$byVar <- NULL
+				inputParamsBy$yLab <- paste(inputParamsBy$yLab, unique(dataBy[, byVar]))
+				do.call(subjectProfileSummaryPlot, inputParamsBy)		
+			})	
+			return(res)
+		}
+	}
 
 	varNotInData <- setdiff(c(meanVar, seVar), colnames(data))
 	if(length(varNotInData) > 0)
@@ -132,19 +159,159 @@ subjectProfileSummaryPlot <- function(data,
 	gg <- gg + theme_classic() + theme(legend.position = "bottom")
 	
 	# set limits for the axes
-	if((!is.null(xlim)) | (!is.null(ylim))){
-		argsCoordCart <- list(xlim = xlim, ylim = ylim)
+	if((!is.null(xLim)) | (!is.null(yLim))){
+		argsCoordCart <- list(xlim = xLim, ylim = yLim)
 		gg <- gg + do.call(coord_cartesian, argsCoordCart)
 	}
 	
-	if(!is.null(xAxisLabs)){
-		fctScaleX <- match.fun(paste0("scale_x_", 
-			ifelse(is.numeric(data[, xVar]), "continuous", "discrete"))
-		)
-		gg <- gg + fctScaleX(breaks = xAxisLabs)
-	}
+	# set limits in the x-axis
+	# even if not specified, to have correct alignment with table
+	if(is.null(xAxisLabs))
+		xAxisLabs <- if(is.factor(data[, xVar]))
+			levels(data[, xVar])	else	unique(data[, xVar])
+	fctScaleX <- if(is.numeric(data[, xVar])){
+		function(breaks)	scale_x_continuous(breaks = breaks, limits = range(breaks))
+	}else function(breaks)
+		function(breaks)	scale_x_discrete(breaks = breaks, drop = FALSE)
+	gg <- gg + fctScaleX(breaks = xAxisLabs)
 	
-	return(gg)
+	res <- if(!is.null(tableText)){
+		
+		# if xLab is specified only consider
+		# the corresponding elements in xVar for the table
+		dataTable <- if(!is.null(xAxisLabs)){
+			data[data[, xVar] %in% xAxisLabs, ]
+		}else	data
+
+		# create plot with table
+		ggTable <- subjectProfileSummaryTable(
+			data = dataTable, xVar = xVar, 
+			text = tableText,
+			xLim = xLim,
+			xLab = xLab,
+			colorVar = colorVar,
+			colorPalette = colorPalette,
+			showLegend = FALSE, yAxisLabs = !is.null(colorVar)
+		)
+		
+		ggTable <- ggTable + fctScaleX(breaks = xAxisLabs)
+
+		# remove legend and title x-axis for base plot (will be included in table plot)
+		gg <- gg + theme(legend.position = "none", axis.title.x = element_blank())
+		
+		# combine base and table plot
+		if(tableHeight > 1 | tableHeight < 0)
+			stop("Table height should be between 0 and 1.")
+		res <- plot_grid(gg, ggTable, ncol = 1,
+			align = "hv", axis = "lr",
+			rel_heights = c(1-tableHeight, tableHeight)
+		)
+		
+	}else gg
+	
+	return(res)
 	
 }
 
+#' Plot a table with \code{ggplot} of a variable of interest 
+#' @param data Data.frame with data for the table.
+#' @param xVar String, variable of \code{data} with variable for the x-axis.
+#' @param label Character vector with colnames of \code{data}
+#' or expression based on colnames of \code{data} to extract
+#' the text label.
+#' @param xLim Vector of the length 2 with limits for the x-axis.
+#' @param colorVar String, variable of \code{data} for coloring.
+#' @param colorLab String, label for \code{colorVar}, used in the legend.
+#' @param colorPalette (named) Vector with color palette.
+#' @param xLab String with label for the x-axis.
+#' @param textSize Size for the text.
+#' @param labelVars Named string with variable labels (names are the variable code).
+#' @param showLegend Logical, should the legend be displayed? TRUE by default.
+#' @param yAxisLabs Logical, if TRUE include the labels in the y-axis.
+#' @param xAxisLabs Vector with labels for the x-axis if \code{xVar}
+#' is discrete or vector with limits if continuous.
+#' @return \code{\link[ggplot2]{ggplot}} object
+#' @import ggplot2
+#' @author Laure Cougnaud
+subjectProfileSummaryTable <- function(
+	data, xVar, text, 
+	xLim = NULL, 
+	colorVar = NULL, colorPalette = NULL,
+	colorLab = getLabelVar(colorVar, labelVars = labelVars),
+	xLab = NULL,
+	textSize = GeomText$default_aes$size,
+	labelVars = NULL,
+	showLegend = TRUE,
+	yAxisLabs = FALSE,
+	xAxisLabs = NULL){
+	
+	data$tableTextLabel <- if(is.expression(text)){
+		eval(expr = text, envir = data)
+	}else if(is.character(text)){
+		if(!text %in% colnames(data)){
+			stop("'tableStats' should be among the columns of 'data'.")
+		}else{
+			data[, text]
+		}
+	}else stop("'text' should be an expression of character.")
+
+	# aesthetics
+	aesTablePlot <- c(
+		list(
+			x = xVar,
+			y = ifelse(!is.null(colorVar), colorVar, 1),
+			label = "tableTextLabel"
+		),
+		if(!is.null(colorVar)) list(color = colorVar)
+	)
+	# arguments for geom_text
+	argsGeomText <- list(
+		mapping = do.call("aes_string", aesTablePlot),
+		size = textSize
+	)
+
+	# base plot
+	ggTable <- ggplot(data = data) + do.call("geom_text", argsGeomText)
+
+	# axis limits
+	if(!is.null(xLim))
+		ggTable <- coord_cartesian(xlim = xLim)
+	
+	# labels
+	if(!is.null(xLab))
+		ggTable <- ggTable + labs(x = xLab)
+	
+	# color palette
+	if(!is.null(colorVar)){
+		if(is.null(colorPalette))
+			colorPalette <- getGLPGColorPalette(x = data[, colorVar])
+		ggTable <- ggTable + scale_color_manual(name = colorLab, values = colorPalette)
+	}
+	
+	if(!is.null(xAxisLabs)){
+		ggTable <- if(is.numeric(data[, xVar])){
+			ggTable + scale_x_continuous(limits = xAxisLabs)
+		}else ggTable + scale_x_discrete(breaks = xAxisLabs)
+	}
+
+	# theme
+	argsTheme <- c(
+		list(
+			axis.ticks.y = element_blank(),
+			axis.title.y = element_blank(),
+			axis.text.x = element_blank(),
+			axis.ticks.x = element_blank()
+		),
+		if(is.null(xLab))	list(axis.title.x = element_blank()),
+		if(!yAxisLabs){
+			list(axis.text.y = element_blank())
+		}else if(!is.null(colorVar)){
+			list(axis.text.y = element_text(colour = colorPalette))
+		},
+		if(!showLegend)	list(legend.position = "none")
+	)
+	ggTable <- ggTable + theme_classic() + do.call(theme, argsTheme)
+	
+	return(ggTable)
+
+}
