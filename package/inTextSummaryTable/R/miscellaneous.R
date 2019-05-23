8b0a30d73f5084d36d3d09add9ee884f6f67fcac
lcougnaud <laure.cougnaud@openanalytics.eu>
2019-05-23 14:20:52 +0200

    inTextSummaryTable: roundCustomText (refs #18607)

--- package/inTextSummaryTable/R/miscellaneous.R
+++ package/inTextSummaryTable/R/miscellaneous.R
@@ -1,34 +1,17 @@
 #' Custom round function, rounding to the closest digits
 #' (instead of rounding to the even number in case of 0.5)
-#' @param x numeric vector to round
-#' @param digits number of digits to consider, 0 by default
-#' @param format string with format for the number: 'text' (with trailing zeros) or 'number'
-#' @return rounded vector
-#' @author stackoverflow question 6461209
-#' @examples
-#' # numbers are rounded to the closest even number in case of .5 
-#' # with the round 'base' function
-#' round(0.45, 1)
-#' # 'roundCustom' round to the closest number
-#' roundCustom(0.45, 1)
-#' # rounding is the same for uneven number:
-#' round(0.55, 1) == roundCustom(0.55, 1)
+#' @param ... Additional parameters for the 
+#' \code{\link[glpgUtilityFct]}{roundCustom}} function.
+#' @param format string with format for the number: 
+#' 'text' (with trailing zeros) (by default) or 'number'.
+#' @inherit glpgUtilityFct::roundCustom return
+#' @importFrom glpgUtilityFct roundCustom
 #' @export
-roundCustom <- function(x, digits = 0, format = c("text", "number")) {
+roundCustomText <- function(..., format = c("text", "number")) {
 	
 	format <- match.arg(format)
 	
-	posneg <- sign(x)
-	z <- abs(x)*10^digits
-	z <- z + 0.5
-	z <- trunc(z)
-	z <- z/10^digits
-	z <- z*posneg
-	
-	res <- switch(format,
-		text = formatC(z, digits = digits, format = "f", flag = "0"),
-		number = z
-	)
+	res <- roundCustomText(..., format = format)
 	
 	return(res)
 	
