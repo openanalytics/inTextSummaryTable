244fd2d2f15cb1ea968cdb8ceb42060dcd2e7c10
Michela Pasetto <mpasetto@openanalytics.eu>
2020-03-31 16:39:56 +0200

    unit tests for formatPercentage and getStats

---
+++ package/inTextSummaryTable/tests/testthat/test_decimals.R
@@ -0,0 +1,13 @@
+context("Test 'formatPercentage'")
+
+test_that("'formatPercentage' function returns correct formatting", {
+			
+			expect_is(class(formatPercentage(x = 45.6)), "character")
+
+			expect_equal(formatPercentage(x = 0.55, nDec = 2), "0.55")
+			expect_equal(formatPercentage(x = 48.43, nDec = 1), "48.4")
+			expect_equal(formatPercentage(x = NA), "-")
+			expect_equal(formatPercentage(x = 0.01), "<0.1")
+			expect_equal(formatPercentage(x = 99.9998), ">99.9")
+			
+		})
