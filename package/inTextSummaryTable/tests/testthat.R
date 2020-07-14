5ae9b0e60b9a5ef76477cf43dd6af60a939f6305
Michela Pasetto <mpasetto@openanalytics.eu>
2020-07-14 16:21:10 +0200

    try setting syst env CI

--- package/inTextSummaryTable/tests/testthat.R
+++ package/inTextSummaryTable/tests/testthat.R
@@ -15,6 +15,8 @@ if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "")
 	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
 test_check(
 		"inTextSummaryTable",
-		reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check"))
+		reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check"),
+		env = Sys.getenv("CI")
+)
 
 
