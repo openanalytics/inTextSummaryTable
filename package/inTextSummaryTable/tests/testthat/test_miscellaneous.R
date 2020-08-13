69ad48c567f772589a4acc2c2d6f2e1bacdc3337
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-08-13 13:03:01 +0200

    tests miscellaneous: set convertVarFlag as internal function

--- package/inTextSummaryTable/tests/testthat/test_miscellaneous.R
+++ package/inTextSummaryTable/tests/testthat/test_miscellaneous.R
@@ -2,10 +2,29 @@ context("Test 'roundCustomText'")
 
 test_that("'roundCustomText' function returns correct rounding", {
 			
-			expect_equal(roundCustomText(x = 4.0, digits = 2), "4.00")
-			expect_equal(roundCustomText(x = 0.55, digits = 1), "0.6")
-			expect_equal(roundCustomText(x = 0.55, digits = 1), as.character(round(0.55, 1)))
+	expect_equal(roundCustomText(x = 4.0, digits = 2), "4.00")
+	expect_equal(roundCustomText(x = 0.55, digits = 1), "0.6")
+	expect_equal(roundCustomText(x = 0.55, digits = 1), as.character(round(0.55, 1)))
+	
+	expect_warning(roundCustomText(x = 4.0, digits = 2, format = "text"))
+	
+})
+
+test_that("Conversion of a flag variable", {
 			
-			expect_warning(roundCustomText(x = 4.0, digits = 2, format = "text"))
+	x <- c("Y", "N", '')	
+	xFL <- inTextSummaryTable:::convertVarFlag(x)
+	expect_is(xFL, "factor")
+	
+	# correct levels
+	expect_equivalent(levels(xFL), c("", "N"))
+	
+	# correct conversions
+	expect_equal(as.character(xFL), c("", "N", NA_character_))
+	
+	# wrong input
+	expect_error(inTextSummaryTable:::convertVarFlag(c(NA_character_, x)), pattern = "*should only contain*")
+	expect_error(inTextSummaryTable:::convertVarFlag(c("blabla", x)), , pattern = "*should only contain*")
 			
-		})
+})
+
