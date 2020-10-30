2b99fad6c9b8034364cb3db4f3defad478ea996e
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-10-30 18:07:14 +0100

    start reformatting unit tests

--- package/inTextSummaryTable/tests/testthat/test_computeSummaryStatistics.R
+++ package/inTextSummaryTable/tests/testthat/test_computeSummaryStatistics.R
@@ -1,239 +1,99 @@
-context("Test 'computeSummaryStatisticsTable' error/warning tracking")
+context("Computation of summary statistics")
 
-dataLB <- ADaMDataPelican$ADLB
-
-# dataset used for the computation of the total (in one of the scenario)
-dataSL <- ADaMDataPelican$ADSL
-dataSL$TRTP <- dataSL$TRT01P 
-
-dataAE <- ADaMDataPelican$ADAE
-
-## tests different error/warning tracking mechanisms
-
-# in case of a summaryTable, 'var' should be specified ...
-expect_error(
-	computeSummaryStatistics(
-		var = NULL,
-		data = subset(dataLB, PARAMCD == "ALB"),
-		type = "summaryTable"
-	)	
-)
-# ... and numeric
-expect_error(
-	computeSummaryStatistics(
-		var = "PARAM",
-		data = subset(dataLB, PARAMCD == "ALB"),
-		type = "summaryTable"
+test_that("stats for continuous variable (including missing) is correctly computed", {
+	
+	dataCont <- data.frame(x = c(NA, 1, 3, 6, 10), USUBJID = sample(10, 5))
+	statsCont <- computeSummaryStatistics(data = dataCont, var = "x")
+	
+	expect_s3_class(statsCont, "data.frame")
+	
+	expect_named(
+		object = statsCont, 
+		expected = c("statN", "statm", "statMean", "statSD", "statSE", "statMedian", "statMin", "statMax"),
+		ignore.order = TRUE
 	)
-)
-
-test_that("No different values of a continuous variable for the same subject ID", {
-			
-	dataSLDupl <- dataSL[1, ]
-	dataSLDupl$AGE <- dataSLDupl$AGE-1
-	dataTable <- rbind(dataSLDupl, dataSL)
-	vars <- "AGE"
-	stats <- getStatsData(
-		data = dataTable,
-		type = 'all',
-		var = vars
-	)$AGE
-	expect_error(
-		summaryTable <- computeSummaryStatisticsTable(
-			data = dataTable, 
-			var = vars,
-			colVar = "TRTP",
-			stats = stats
-		),
-		pattern = "multiple records are available"
-	)
+	
+	expect_equal(statsCont[, "statN"], 4)
+	expect_equal(statsCont[, "statm"], 4)	
+	expect_equal(statsCont[, "statMean"], 5)
+	expect_equal(statsCont[, "statMedian"], 4.5)
+	expect_equal(statsCont[, "statSD"], sd(dataCont$x, na.rm = TRUE))
+	expect_equal(statsCont[, "statSE"], statsCont[, "statSD"]/sqrt(statsCont[, "statm"]))
+	expect_equal(statsCont[, "statMin"], 1)
+	expect_equal(statsCont[, "statMax"], 10)
 	
 })
 
-test_that("Filtering of duplicated records for the same subject ID for continuous variable", {
+test_that("stats for categorical variable (including missing) is correctly computed", {
 			
-	treats <- unique(dataSL$TRTP)
-	if(length(treats) > 1){
-		
-		dataTreat1 <- subset(dataSL, TRTP == treats[1])
-		
-		dataDupl <- subset(dataTreat1, USUBJID == dataTreat1[1, "USUBJID"])
-		dataDupl$TRTP <- treats[2]
-		
-		dataTable <- rbind(dataDupl, dataSL)
-		
-		vars <- "AGE"
-		stats <- getStatsData(
-			data = dataTable,
-			type = 'all',
-			var = vars
-		)$AGE
-
-		expect_message(
-			summaryTableDupl <- computeSummaryStatisticsTable(
-				data = dataTable, 
-				var = vars,
-				colVar = "TRTP",
-				stats = stats,
-				colTotalInclude = TRUE
-			),
-			regexp = "duplicated values for AGE are filtered"
-		)
-
-		summaryTableInit <- computeSummaryStatisticsTable(
-			data = dataSL, 
-			var = vars,
-			colVar = "TRTP",
-			stats = stats,
-			colTotalInclude = TRUE
-		)
-		
-		expect_identical(
-			object = subset(summaryTableDupl, TRTP == "Total" & !isTotal),
-			expected = subset(summaryTableInit, TRTP == "Total" & !isTotal)
-		)
-
-	}
+	dataCat <- data.frame(x = c(NA_character_, "B", "B", "B", "A"), USUBJID = sample(10, 5))
+	statsCat <- computeSummaryStatistics(data = dataCat, var = "x")
+			
+	expect_s3_class(statsCat, "data.frame")
+			
+	expect_named(
+		object = statsCat, 
+		expected = c("variableGroup", "statN", "statm"),
+		ignore.order = TRUE
+	)
+			
+	expect_s3_class(statsCat[, "variableGroup"], "factor")
+	expect_identical(statsCat[, "variableGroup"], factor(c("A", "B")))
 	
+	expect_equal(statsCat[, "statm"], c(1, 3))
+	expect_equal(statsCat[, "statN"], c(1, 3))
+
 })
 
-test_that("More columns in dataTotalRow than in data to summarize", {
-		
-	treats <- unique(dataAE$TRTA)
-	dataTable <- subset(dataAE, !TRTA %in% treats[1])
+test_that("stats for empty variable returns results only if 'filterData' is specified", {
+			
+	emptyData <- data.frame(x = numeric(), USUBJID = character())
 	
-	dataTotalRow <- list(AEDECOD = {
-		ddply(dataAE, c("USUBJID", "TRTA"), function(x){
-			x[which.max(x$AESEVN), ]
-		})
-	})
+	# without filter
+	expect_null(computeSummaryStatistics(data = emptyData, var = "x"))
 	
 	expect_silent(
-		getSummaryStatisticsTable(
-			data = dataTable,
-			colVar = "TRTA",
-			rowVar = c("AEDECOD", "AESEV"),
-			rowVarInSepCol = "AESEV",
-			rowVarTotalInclude = "AEDECOD",
-			stats = getStats("n (%)"),
-			dataTotalRow = dataTotalRow,
-			rowVarTotalByVar = "AESEV"
+		statsEmptyData <- computeSummaryStatistics(
+			data = data.frame(x = numeric(), USUBJID = character()), 
+			var = "x",
+			filterEmptyVar = FALSE
 		)
 	)
+	expect_s3_class(statsEmptyData, "data.frame")
+	expect_named(
+		object = statsEmptyData, 
+		expected = c("statN", "statm", "statMean", "statSD", "statSE", "statMedian", "statMin", "statMax"),
+		ignore.order = TRUE
+	)
+	
+})
+
+test_that("failure if variable is not available", {
+	expect_error(
+		computeSummaryStatistics(
+			data = data.frame(x = 1:10), 
+			var = "y"
+		),
+		pattern = "Variable to summarize .* not available"
+	)		
+})
 			
+
+test_that("failure if subjectVar is not available", {
+	expect_error(
+		computeSummaryStatistics(
+			data = data.frame(x = 1:10), 
+			var = "x", 
+			subjectVar = "blabla"
+		),
+		pattern = "Subject variable .* not available"
+	)		
 })
 
-test_that("Custom set of statistics per variable", {
-
-	# variable to summarize
-	stats <- list(
-		AVAL = getStats(c("n", "mean (se)")), 
-		CHG = getStats(c("n", "Median", "mean (se)"))
-	)
-	summaryTable <- computeSummaryStatisticsTable(
-		var = c("AVAL", "CHG"),
-		data = subset(dataLB, PARAMCD == "ALB" & AN01FL == "Y"),
-		colVar = c("TRTP", "AVISIT"),
-		stats = stats
-	)
-	expect_identical(
-		attr(summaryTable, "summaryTable")$statsVar,
-		c("n", "Median", "Mean (SE)"),
-		label = "Order of statistics is as specified by variable"
-	)
-	
-	# extra variable
-	dataTable <- subset(dataLB, PARAMCD %in% c("ALB", "ALP") & AN01FL == "Y")
-	stats <- list(
-		AVAL = list(
-			ALB = getStats("n"), 
-			ALP = getStats(c("n", "mean (se)"))
-		), 
-		CHG = list(
-			ALB = getStats(c("n", "Median", "mean (se)")),
-			ALP = getStats(c("n", "Median", "mean (se)"))
-		)
-	)
-	summaryTable <- computeSummaryStatisticsTable(
-		rowVar = "PARAMCD",
-		var = c("AVAL", "CHG"),
-		data = dataTable,
-		colVar = c("TRTP", "AVISIT"),
-		stats = stats, statsVarBy = "PARAMCD"
-	)
-	expect_identical(
-		attr(summaryTable, "summaryTable")$statsVar,
-		c("n", "Median", "Mean (SE)"),
-		label = "Order of statistics is as specified by parameter and variable"
-	)
-	
-})
-
-test_that("Order of summary statistic variable is correct", {
-		
-	# If variable is a character, levels should be sorted in alphabetical order
-	dataSL$SEX <- as.character(dataSL$SEX)
-	tableCharac <- computeSummaryStatisticsTable(data = dataSL, var = "SEX")
-	expect_identical(object = levels(tableCharac$variableGroup), expected = sort(unique(dataSL$SEX)))
-	
-	# If variable is a factor, levels order should be retained
-	dataSL$SEX <- factor(dataSL$SEX, levels = rev(sort(unique(dataSL$SEX))))
-	tableFactor <- computeSummaryStatisticsTable(data = dataSL, var = "SEX")
-	expect_identical(object = levels(tableFactor$variableGroup), expected = levels(dataSL$SEX))
-	
-	# even if not present for all colVar
-	dataSL$TRT01P <- factor(dataSL$TRT01P)
-	dataSL$SEX <- factor(dataSL$SEX, levels = rev(sort(unique(dataSL$SEX))))
-	# subset to retain:
-	# only last element of SEX for first TRT01P (will be computed first)
-	# the first element of SEX for the other TRT01P
-	# to check that order of SEX is computed upfront grouping by TRT01P
-	dataSLSubset <- subset(dataSL, (
-			TRT01P == head(levels(dataSL$TRT01P), 1) &
-			SEX %in% tail(levels(dataSL$SEX), 1)
-		) | 
-		!TRT01P == head(levels(dataSL$TRT01P), 1) &
-		!SEX %in% tail(levels(dataSL$SEX), 1)
-	)
-#	with(dataSLSubset, table(TRT01P, SEX))
-	tableFactorNotComplete <- computeSummaryStatisticsTable(data = dataSLSubset, var = "SEX")
-	expect_identical(object = levels(tableFactorNotComplete$variableGroup), expected = levels(dataSLSubset$SEX))
-	
-	# even if the var is character
-	dataSLSubset$SEX <- as.character(dataSLSubset$SEX)
-	tableCharacNotComplete <- computeSummaryStatisticsTable(data = dataSLSubset, var = "SEX")
-	expect_identical(object = levels(tableCharacNotComplete$variableGroup), expected = sort(unique(dataSLSubset$SEX)))
-	
-})
-
-test_that("Variable name is included when needed", {
-			
-	# no variable:
-	descTableNoVar <- computeSummaryStatisticsTable(data = dataSL)
-	expect_false("variable" %in% colnames(descTableNoVar))	
-	expect_warning(
-		descTableNoVar <- computeSummaryStatisticsTable(data = dataSL, varLabInclude = TRUE),
-		regexp = "Variable label is not included"
-	)
-	
-	# one variable
-	descTableOneVar <- computeSummaryStatisticsTable(data = dataSL, var = "AGE")
-	expect_false("variable" %in% colnames(descTableOneVar))		
-	descTableOneVarWithLabel <- computeSummaryStatisticsTable(
-		data = dataSL, var = "AGE", varLabInclude = TRUE
-	)
-	expect_true("variable" %in% colnames(descTableOneVarWithLabel))		
-	
-	# > 1 variable
-	descTableMoreOneVar <- computeSummaryStatisticsTable(data = dataSL, var = c("AGE", "SEX"))
-	expect_true("variable" %in% colnames(descTableMoreOneVar))		
-	expect_warning({
-		descTableMoreOneVarWithLabel <- computeSummaryStatisticsTable(
-			data = dataSL, var = c("AGE", "SEX"), varLabInclude = FALSE
-		)
-		},
-		regexp = "Variable label is included"
-	)
-	expect_true("variable" %in% colnames(descTableMoreOneVarWithLabel))
-	
+test_that("failure if multiple records available per subject", {
+	dataCont <- data.frame(x = seq.int(5), USUBJID = c(1, 1, 2, 2, 2))
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x"),
+		pattern = "Extraction of statistics failed .* because multiple records are available"
+	)		
 })
