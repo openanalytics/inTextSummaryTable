7dfce9257e8ba960a1756fa00b3eef69db34d74f
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-11-02 12:14:38 +0100

    add unit tests for computeSummaryStatistics

---
+++ package/inTextSummaryTable/R/computeSummaryStatistics.R
@@ -0,0 +1,293 @@
+#' Compute summary statistics of interest of an unique variable of interest.
+#' 
+#' Additionally, this function run extra checks on the data:
+#' \itemize{
+#' \item{an error message is triggered if any subject (identified by \code{subjectVar}) 
+#' have different values in a continuous \code{var}
+#' }
+#' \item{an indicative message is triggered if multiple but identical records are available
+#' for \code{subjectVar} and a continuous \code{var}}
+#' } 
+#' @param data Data.frame with data.
+#' @param var String, variable of \code{data} with variable to compute statistics on.
+#' Missing values, if present, are filtered.
+#' If NULL or 'all', counts in the entire \code{data} are computed.
+#' @param subjectVar String, variable of \code{data} with subject ID,
+#' 'USUBJID' by default.
+#' @param type String with type of table: 
+#' \itemize{
+#' \item{'summaryTable': }{summary table with custom statistics}
+#' \item{'countTable': }{count table}
+#' \item{'auto' (by default): }{'summaryTable' of \code{var} is numeric,
+#' 'countTable' otherwise
+#' }
+#' }
+#' @param filterEmptyVar Logical, if TRUE doesn't return any results
+#' if the variable is empty, otherwise return 0 for the counts and NA for summary 
+#' statistics.
+#' Criterias to consider a variable empty are:
+#' \itemize{
+#' \item{for a continuous variable: }{all missing (NA)}
+#' \item{for a categorical variable: }{all missing or **category is included in the 
+#' factor levels but not available in \code{data}**}
+#' }
+#' By default: an empty variable is filtered for a continuous variable
+#' (\code{type} is 'summaryTable' or [\code{type} is 'auto' and 'var' is a numeric variable],
+#' and retained (including all levels) for a categorical variable.
+#' @param varTotalInclude Logical (FALSE by default)
+#' Should the total across all categories of \code{var} 
+#' be included for the count table?
+#' Only used if \code{var} is a categorical variable.
+#' @param statsExtra (optional) Named list with functions for additional custom
+#' statistics to be computed, only available for 'summaryTable',
+#' e.g. list(statCVPerc = function(x) sd(x)/mean(x)*100) (or \code{\link{cv}}).
+#' Each function has as parameter: either 'x': the variable or 'data': the entire dataset,
+#' and return the corresponding summary statistic.
+#' @param msgLabel (optional) String with label for the data (NULL by default), 
+#' included in the message/warning for checks.
+#' @param msgVars (optional) Character vector with columns of \code{data}
+#' containing extra variables (besides \code{var} and \code{subjectVar})
+#' that should be included in the message/warning for checks.
+#' @return Data.frame with summary statistics in columns,
+#' depending if \code{type} is:
+#' \itemize{
+#' \item{'summary': }{
+#' \itemize{
+#' \item{'statN': }{number of subjects}
+#' \item{'statm': }{number of records}
+#' \item{'statMean': }{mean of \code{var}}
+#' \item{'statSD': }{standard deviation of \code{var}}
+#' \item{'statSE': }{standard error the mean of \code{var}}
+#' \item{'statMedian': }{median of \code{var}}
+#' \item{'statMin': }{minimum of \code{var}}
+#' \item{'statMax': }{maximum of \code{var}}
+#' }
+#' }
+#' \item{'count': }{
+#' \itemize{
+#' \item{'variableGroup': }{factor with groups of \code{var} for which counts are reported}
+#' \item{'statN': }{number of subjects}
+#' \item{'statm': }{number of records}
+#' }
+#' }
+#' }
+#' @author Laure Cougnaud
+#' @importFrom stats na.omit median sd
+#' @importFrom methods formalArgs
+#' @importFrom utils capture.output hasName
+#' @export
+computeSummaryStatistics <- function(data, 
+	var = NULL, varTotalInclude = FALSE,
+	statsExtra = NULL,
+	subjectVar = "USUBJID",
+	filterEmptyVar = (
+		(type == "auto" && var != "all" && is.numeric(data[, var])) | 
+		type == "summaryTable"
+	),
+	type = "auto",
+	msgLabel = NULL, msgVars = NULL){
+	
+	## checks parameters
+	
+	type <- match.arg(type, choices = c("auto", "summaryTable", "countTable"))
+	
+	if(!is.null(var) && var != "all" && !var %in% colnames(data))
+		stop(paste("Variable to summarize:", shQuote(var), "is not available in data."))
+	
+	if(!subjectVar %in% colnames(data))
+		stop(paste("Subject variable:", shQuote(subjectVar), 
+			"is not available for the computation of the number of subjects."))
+	
+	if(type == "auto")
+		type <- ifelse(
+			!is.null(var) &&  var != "all" && is.numeric(data[, var]), 
+			"summaryTable", 
+			"countTable"
+		)
+	
+	if(type == "summaryTable"){
+		if(is.null(var)){
+			stop("Variable of interest should be specified via the 'var' parameter for a summary table.")
+		}else if(var == "all"){
+			stop("The 'type' should be set to 'countTable' in case 'var' is 'all'.")
+		}else	if(!is.numeric(data[, var])){
+			stop("Variable of interest: 'var' should be numeric in case type is set to 'summaryTable'.")
+		}
+	}
+	
+	if(!is.null(var) && var != "all")
+		data <- data[!is.na(data[, var]), , drop = FALSE]
+	
+	getNSubjects <- function(x){
+		as.integer(n_distinct(x[, subjectVar]))
+	}
+	getNRecords <- function(x) nrow(x)
+	
+	# wrapper to add extra statistics
+	statsExtraFct <- function(res, statsExtra, val = NULL, data){
+		# Note: error with: 'cbind' if res is empty
+		# so only add stats if some results are available
+		if(!is.null(statsExtra) & nrow(res) > 0){
+			
+			if(!is.list(statsExtra))
+				stop("'statsExtra' should be a list.")
+			
+			if(is.null(names(statsExtra)) || !all(names(statsExtra) != ""))
+				stop("'statsExtra' should be named.")
+			
+			statsExtraCommon <- intersect(names(statsExtra), colnames(res))
+			if(length(statsExtraCommon) > 0)
+				stop("Name(s) in 'statsExtra': ", toString(statsExtraCommon), 
+					" are already in default statistics, please choose a different name for your custom statistics.")
+			resExtra <- sapply(statsExtra, function(fct){
+				switch(
+					formalArgs(fct)[1], 
+					'x' = fct(val), 
+					'data' = fct(data),
+					stop("'statsExtra' should contain a parameter named 'x' or 'data'.")
+				)
+			}, simplify = FALSE)	
+			
+			res <- cbind(res, resExtra, stringsAsFactors = TRUE)
+		}
+		return(res)
+	}
+	
+	switch(type,
+			
+		'summaryTable' = {
+				
+			val <- data[, var]
+			emptyVar <- is.null(val) || length(val) == 0
+			if(!(filterEmptyVar & emptyVar)){
+				
+				# check of multiple records per subject
+				if(!emptyVar){
+					
+					# filter records with same values per subject:
+					dataSubjVar <- data[, c(subjectVar, var)]
+					isDuplSubjVar <- duplicated(dataSubjVar)
+					if(any(isDuplSubjVar) > 0){
+						dataDupl <- merge(data, data[isDuplSubjVar, c(subjectVar, var), drop = FALSE])
+						dataDupl <- dataDupl[, unique(c(subjectVar, msgVars, var)), drop = FALSE]
+						message(sum(isDuplSubjVar), " record(s) with duplicated values",
+							" for ", var, 
+							" are filtered before the computation of the statistics ", 
+							if(!is.null(msgLabel))	paste("for the", msgLabel), ":\n",
+							paste(capture.output(print(dataDupl)), collapse = "\n")
+						)
+						data <- data[!isDuplSubjVar, ]
+						val <- data[, var]
+					}
+					
+					isDupl <- duplicated(data[, subjectVar])
+					if(any(isDupl)){
+						dataDupl <- merge(data, data[isDupl, subjectVar, drop = FALSE])
+						dataDupl <- dataDupl[, unique(c(subjectVar, msgVars, var)), drop = FALSE]
+						stop("Extraction of statistics failed for ", var,
+							if(!is.null(msgLabel))	paste(" for the", msgLabel), 
+							" because multiple records are available for ",
+							"the same ", subjectVar, ":\n",
+							paste(capture.output(print(dataDupl)), collapse = "\n")
+						)
+					}
+				}
+				
+				res <- data.frame(
+					statN = getNSubjects(data),
+					statm = getNRecords(data),
+					statMean = ifelse(emptyVar, NA_real_, mean(val)),
+					statSD = ifelse(emptyVar, NA_real_, sd(val)),
+					statSE = ifelse(emptyVar, NA_real_, se(val)),
+					statMedian = ifelse(emptyVar, NA_real_, median(val)),
+					statMin = ifelse(emptyVar, NA_real_, min(val)),
+					statMax = ifelse(emptyVar, NA_real_, max(val))
+				)
+				res <- statsExtraFct(
+					res = res, statsExtra = statsExtra, 
+					val = val, data = data
+				)
+				
+			}else	res <- NULL
+			
+		},
+		
+		'countTable' = {
+			
+			# to avoid that ddply with empty data returns entire data.frame
+			if(nrow(data) == 0){
+				
+				if(!is.null(var)){
+					res <- data.frame()
+				}else{
+					res <- data.frame(statN = 0, statm = 0)
+				}
+				res <- statsExtraFct(
+					res = res, statsExtra = statsExtra,
+					data = data
+				)
+				
+			}else{
+				
+				if(!is.null(var) && var != "all"){
+					
+					varLevels <- if(is.factor(data[, var]))	levels(data[, var])	else	unique(data[, var])
+					resList <- lapply(varLevels, function(level){
+						x <- data[which(data[, var] == level), ]	
+						# compute stats in data or if filterEmptyVar is FALSE
+						if(!(nrow(x) == 0 & filterEmptyVar)){
+							res <- setNames(
+									data.frame(level, getNSubjects(x), getNRecords(x), stringsAsFactors = FALSE),
+									c(var, "statN", "statm")
+							)
+							res <- statsExtraFct(
+									res = res, statsExtra = statsExtra,
+									data = x
+							)
+						}
+					})
+					resList <- resList[!sapply(resList, is.null)]
+					if(length(resList) > 0){
+						res <- do.call(rbind, c(resList, stringsAsFactors = FALSE))
+						res[, var] <- factor(res[, var], levels = varLevels)
+						rownames(res) <- NULL
+					}else{
+						res <- data.frame()
+					}
+					
+				}else{
+					res <- data.frame(statN = getNSubjects(data), statm = getNRecords(data))
+					res <- statsExtraFct(res = res, statsExtra = statsExtra, data = data)
+				}
+				
+				includeTotal <- varTotalInclude & 
+					(!(filterEmptyVar & nrow(data) == 0)) & 
+					(is.null(var) || var != "all")
+				if(includeTotal){
+					
+					resTotal <- data.frame(
+						statN = getNSubjects(data),
+						statm = getNRecords(data)
+					)
+					resTotal <- statsExtraFct(res = resTotal, statsExtra = statsExtra, data = data)
+					resTotal[, var] <- "Total"
+					res <- rbind.fill(res, resTotal)
+					elVar <- if(is.factor(data[, var]))	levels(data[, var])	else	unique(data[, var])
+					res[, var] <- factor(res[, var], levels = c(elVar, "Total"))
+					
+				}
+				
+				if(is.null(var) || var == "all"){
+					res[, ".id"] <- NULL
+				}else{
+					colnames(res)[match(var, colnames(res))] <- "variableGroup"
+				}
+			}
+			
+		}
+	
+	)
+	
+	return(res)
+	
+}
--- package/inTextSummaryTable/tests/testthat/test_computeSummaryStatistics.R
+++ package/inTextSummaryTable/tests/testthat/test_computeSummaryStatistics.R
@@ -1,5 +1,7 @@
 context("Computation of summary statistics")
 
+set.seed(123)
+
 test_that("stats for continuous variable (including missing) is correctly computed", {
 	
 	dataCont <- data.frame(x = c(NA, 1, 3, 6, 10), USUBJID = sample(10, 5))
@@ -45,39 +47,25 @@ test_that("stats for categorical variable (including missing) is correctly compu
 
 })
 
-test_that("stats for empty variable returns results only if 'filterData' is specified", {
+
+test_that("variable is properly checked", {
 			
-	emptyData <- data.frame(x = numeric(), USUBJID = character())
+	dataCat <- data.frame(x = c(NA_character_, "B", "B", "B", "A"), USUBJID = sample(10, 5))
 	
-	# without filter
-	expect_null(computeSummaryStatistics(data = emptyData, var = "x"))
+	# empty var
+	expect_error(statsVarNULL <- computeSummaryStatistics(data = dataCat, var = NULL), NA)
+	expect_error(statsVarAll <- computeSummaryStatistics(data = dataCat, var = "all"), NA)
+	expect_identical(statsVarNULL, statsVarAll)
+	expect_equal(statsVarNULL, data.frame(statN = 5, statm = 5))
 	
-	expect_silent(
-		statsEmptyData <- computeSummaryStatistics(
-			data = data.frame(x = numeric(), USUBJID = character()), 
-			var = "x",
-			filterEmptyVar = FALSE
-		)
-	)
-	expect_s3_class(statsEmptyData, "data.frame")
-	expect_named(
-		object = statsEmptyData, 
-		expected = c("statN", "statm", "statMean", "statSD", "statSE", "statMedian", "statMin", "statMax"),
-		ignore.order = TRUE
+	# var not in data
+	expect_error(
+		computeSummaryStatistics(data = dataCat, var = "y"),
+		"Variable to summarize.*not available in data."
 	)
 	
 })
 
-test_that("failure if variable is not available", {
-	expect_error(
-		computeSummaryStatistics(
-			data = data.frame(x = 1:10), 
-			var = "y"
-		),
-		pattern = "Variable to summarize .* not available"
-	)		
-})
-			
 
 test_that("failure if subjectVar is not available", {
 	expect_error(
@@ -97,3 +85,169 @@ test_that("failure if multiple records available per subject", {
 		pattern = "Extraction of statistics failed .* because multiple records are available"
 	)		
 })
+
+test_that("total included if requested", {
+		
+	dataCat <- data.frame(x = c(NA_character_, "B", "B", "B", "A"), USUBJID = sample(10, 5))
+	statsCatWithTotal <- computeSummaryStatistics(data = dataCat, var = "x", varTotalInclude = TRUE)
+	
+	statsCat <- computeSummaryStatistics(data = dataCat, var = "x", varTotalInclude = FALSE)
+	statsCatIncludeTotal <- rbind(statsCatWthtTotal, data.frame(variableGroup = "Total", statN = 4, statm = 4))
+	
+	expect_equal(object = statsCatWithTotal, expected = statsCatIncludeTotal)
+			
+})
+
+test_that("extra statistics correctly included", {
+			
+	dataCont <- data.frame(x = c(NA, 1, 3, 6, 10), USUBJID = sample(10, 5))
+	
+	# create example of custom function (coefficient of variation)
+	statsFctX <- function(x) sd(x, na.rm = TRUE)/mean(x, na.rm = TRUE)*100
+	statsFctData <- function(data) sd(data$x, na.rm = TRUE)/mean(data$x, na.rm = TRUE)*100
+	
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", statsExtra = statsFctX),
+		"should be a list",
+	)
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(statsFctX)),
+		"should be named"
+	)
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(A = statsFctX, 2)),
+		"should be named"
+	)
+	
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(statN = statsFctX)),
+		"choose a different name"
+	)
+	
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(A = function(y) length(y))),
+		"'statsExtra' should contain a parameter named 'x' or 'data'"
+	)
+	
+	expect_silent(statsExtraFctX <- computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(A = statsFctX)))
+	expect_true("A" %in% names(statsExtraFctX))
+	expect_equal(statsExtraFctX$A, statsFctX(dataCont$x))
+	
+	expect_silent(statsExtraFctX <- computeSummaryStatistics(data = dataCont, var = "x", statsExtra = list(B = statsFctData)))
+	expect_true("B" %in% names(statsExtraFctX))
+	expect_equal(statsExtraFctX$B, statsFctData(dataCont))
+	
+})
+
+test_that("statistic type is correct for categorical variable", {
+		
+	dataCat <- data.frame(x = c(NA_character_, "B", "B", "B", "A"), USUBJID = sample(10, 5))
+	expect_error(
+		stats <- computeSummaryStatistics(data = dataCat, var = "x", type = "summaryTable"),
+		"should be numeric"
+	)
+	expect_identical(
+		computeSummaryStatistics(data = dataCat, var = "x", type = "auto"),
+		computeSummaryStatistics(data = dataCat, var = "x", type = "countTable")
+	)
+	
+})
+
+test_that("statistic type is correct for continuous variable", {
+			
+	dataCont <- data.frame(x = c(NA, 1, 3, 6, 10), USUBJID = sample(10, 5))
+	expect_identical(
+		computeSummaryStatistics(data = dataCont, var = "x", type = "auto"),
+		computeSummaryStatistics(data = dataCont, var = "x", type = "summaryTable")
+	)
+	
+	# count table can be requested for a continuous variable
+	expect_error(computeSummaryStatistics(data = dataCont, var = "x", type = "countTable"), NA)
+			
+})
+
+test_that("filtering of empty continuous variable correctly done", {
+	
+	# variable is considered empty if all missing:
+	emptyDataNA <- data.frame(x = rep(NA_real_, 5), USUBJID = sample(10, 5))	
+	expect_null(computeSummaryStatistics(data = emptyDataNA, var = "x"))
+	expect_silent(
+		statsEmptyDataNA <- computeSummaryStatistics(data = emptyDataNA, var = "x", filterEmptyVar = FALSE)
+	)
+	expect_equal(nrow(statsEmptyDataNA), 1)
+	expect_equal(
+		statsEmptyDataNA, 
+		data.frame(statN = 0, statm = 0, statMean = NA_real_, 
+			statSD = NA_real_, statSE = NA_real_, statMedian = NA_real_, 
+			statMin = NA_real_, statMax = NA_real_
+		)
+	)
+	
+	# variable is empty is data.frame is itself empty
+	emptyData <- data.frame(x = numeric(), USUBJID = character())
+	expect_silent(
+		statsEmptyData <- computeSummaryStatistics(data = emptyData, var = "x", filterEmptyVar = FALSE)
+	)
+	expect_equal(statsEmptyData, statsEmptyDataNA)
+	
+})	
+
+test_that("filtering of empty categorical variable correctly done", {
+			
+	dataCat <- data.frame(
+		x = factor(c(NA_character_, "B", "B", "B", "A"), levels = c("A", "B", "C")),
+		USUBJID = sample(10, 5)
+	)
+			
+	# filter
+	expect_equal(nrow(computeSummaryStatistics(data = dataCat, var = "x", filterEmptyVar = TRUE)), 2)
+	
+	# no filter
+	statsCatNoFilter <- computeSummaryStatistics(data = dataCat, var = "x", filterEmptyVar = FALSE)
+	expect_equal(nrow(statsCatNoFilter), 3)
+	expect_true("C" %in% statsCatNoFilter$variableGroup)
+	expect_equal(subset(statsCatNoFilter, variableGroup == "C")$statN, 0)
+	expect_equal(subset(statsCatNoFilter, variableGroup == "C")$statm, 0)
+	expect_identical(computeSummaryStatistics(data = dataCat, var = "x"), statsCatNoFilter)
+	
+})
+
+test_that("check for duplicates in continuous variable correctly done", {
+			
+	dataCont <- data.frame(
+		x = c(NA, 1, 3, 6, 10), 
+		y = rep(2, 5),
+		USUBJID = c("A", "B", "B", "C", "D")
+	)
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x"),
+		"multiple records are available for the same USUBJID"
+	)
+	
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", msgVars = "y"),
+		"multiple records are available for the same USUBJID.* USUBJID y x"
+	)
+	
+	expect_error(
+		computeSummaryStatistics(data = dataCont, var = "x", msgLabel = "test dataset"),
+		"for the test dataset .* multiple records are available for the same USUBJID"
+	)
+			
+})
+
+test_that("check for multiple values in continuous variable correctly done", {
+			
+	dataCont <- data.frame(
+		x = c(NA, 1, 1, 6, 10), 
+		USUBJID = c("A", "B", "B", "C", "D")
+	)
+			
+	expect_message(
+		computeSummaryStatistics(data = dataCont, var = "x"),
+		"duplicated values for x"
+	)
+	
+})
+
+	
