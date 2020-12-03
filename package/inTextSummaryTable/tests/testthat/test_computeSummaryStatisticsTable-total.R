b8591ec9f52ac9d0697c1b9b95c7d154d9e1b1e5
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-12-03 18:23:41 +0100

    computeSummaryStatisticsTable: unit tests for total dataset for percentage and row var (in progress)

--- package/inTextSummaryTable/tests/testthat/test_computeSummaryStatisticsTable-total.R
+++ package/inTextSummaryTable/tests/testthat/test_computeSummaryStatisticsTable-total.R
@@ -31,7 +31,7 @@ test_that("general total is extracted by default from data", {
 			
 })
 
-test_that("a different dataset for the general total is specified", {
+test_that("general total is is extracted from specified dataset", {
 			
 	# data with different number of subjects/records per treatment
 	data <- data.frame(
@@ -85,5 +85,99 @@ test_that("a different dataset for the general total is specified", {
 	expect_match(w, ".*colVarTotal are ignored because they are not available in: total dataset.", all = FALSE)
 	expect_match(w, ".*colVarTotalPerc are ignored because they are not available in: total dataset for percentage.", all = FALSE)
 	expect_equal(sum(sumTable$isTotal), 1) # total computed across columns
+	
+})
+
+test_that("percentage is extracted from specified dataset", {
+
+	# data with different number of subjects/records per treatment
+	data <- data.frame(
+		USUBJID = c(1, 1, 2, 3, 4, 4, 5),
+		TRT = rep(c("A", "B"), times = c(4, 3)),
+		ADECOD = c("a", "b", "a", "c", "d", "a", "b"),
+		stringsAsFactors = FALSE
+	)	
+	
+	dataTotalPerc <- data.frame(
+		TRT = rep(c("A", "B"), c(6, 4)),
+		USUBJID = c(c(1, seq.int(5)), c(1, seq.int(3)))
+	)
+	
+	## correct spec
+	
+	sumTable <- computeSummaryStatisticsTable(
+		data = data,
+		var = "ADECOD",
+		colVar = "TRT",
+		dataTotalPerc = dataTotalPerc
+	)
+	
+	# percentage is computed from the specified dataset
+	expect_true(all(subset(sumTable, TRT == "A")$statPercTotalN == 5))
+	expect_true(all(subset(sumTable, TRT == "B")$statPercTotalN == 3))
+	
+	expect_equal(sumTable$statPercN, with(sumTable, statN/statPercTotalN)*100)
+	
+	# data total is still extracted from the dataset directly
+	sumTableTotal <- subset(sumTable, isTotal)
+	sumTableTotal <- sumTableTotal[match(c("A", "B"), sumTableTotal$TRT), ]
+	
+	# # subjects/records for total is correct
+	expect_equal(sumTableTotal$statN, c(3, 2))
+	expect_equal(sumTableTotal$statm, c(4, 3))
+	
+	## wrong spec
+	dataTotalPerc <- data.frame(
+		USUBJID = c(c(1, seq.int(5)), c(1, seq.int(3)))
+	)
+	expect_warning(
+		sumTable <- computeSummaryStatisticsTable(
+			data = data,
+			var = "ADECOD",
+			colVar = "TRT",
+			dataTotalPerc = dataTotalPerc
+		),
+		".*colVarTotalPerc are ignored because they are not available in: total dataset for percentage."
+	)
+})
+
+test_that("a different dataset for the row total is specified", {
+			
+	# data with different number of subjects/records per treatment
+	data <- data.frame(
+		USUBJID = c(1, 1, 2, 3, 4, 4, 5),
+		TRT = rep(c("A", "B"), times = c(4, 3)),
+		ABODSYS = c("AB", "AB", "AB", "CD", "CD", "AB", "AB"),
+		ADECOD = c("a", "b", "a", "c", "d", "a", "b"),
+		stringsAsFactors = FALSE
+	)
+	
+	## TODO: correct spec
+	
+	## incorrect spec
+	
+	# variables (columns, rows) missing in dataTotalRow
+	expect_error(
+		computeSummaryStatisticsTable(
+			data = data,
+			colVar = "TRT",
+			rowVar = c("ABODSYS", "ADECOD"),
+			rowVarTotalInclude = c("ABODSYS", "ADECOD"),
+			dataTotalRow = data[, "USUBJID", drop = FALSE]
+		),
+		"*not available.*dataset for row total"
+	)
+	
+	# dataset specified as a list but not for all variables
+	expect_error(
+		computeSummaryStatisticsTable(
+			data = data,
+			colVar = "TRT",
+			rowVar = c("ABODSYS", "ADECOD"),
+			rowVarTotalInclude = c("ABODSYS", "ADECOD"),
+			dataTotalRow = list(ABODSYS = data)
+		),
+		"Dataset for row total missing for:.*ADECOD.*"
+	)
 	
 })
