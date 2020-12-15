bc30ac64742d70e2a65f7f327ed369264280fe83
lcougnaud <laure.cougnaud@openanalytics.eu>
2020-12-15 18:46:08 +0100

    exportSummaryStatisticsTable: unit tests flextable export (part 2)

--- package/inTextSummaryTable/tests/testthat/test_exportSummaryStatisticsTable-flextable.R
+++ package/inTextSummaryTable/tests/testthat/test_exportSummaryStatisticsTable-flextable.R
@@ -42,6 +42,28 @@ test_that("table is exported to flextable with label for row variables", {
 			
 })
 
+test_that("table is exported to flextable with label for row variables extracted from label vars", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		PARAM = factor(c("A", "B"), levels = c("B", "A")),
+		n = c(9, 10)
+	)
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				rowVar = "PARAM",
+				labelVars = c(PARAM = "Parameter")
+			)
+			ft$header$dataset[, 1]
+		}, 
+		expected = "Parameter"
+	)
+	
+})
+
+
 test_that("table is exported to flextable with row variables in a separated column", {
 			
 	set.seed(123)
@@ -64,7 +86,7 @@ test_that("table is exported to flextable with row variables in a separated colu
 	
 })
 
-test_that("table is exported to flextable with row variables in a separated column", {
+test_that("table is exported to flextable with specified row formatting", {
 			
 	set.seed(123)
 	summaryTable <- data.frame(
@@ -114,7 +136,7 @@ test_that("table is exported to flextable with total included in the header row"
 	
 })
 
-test_that("table is exported to flextable with total included in a separated row", {
+test_that("table is exported to flextable with row total included in a separated row", {
 			
 	set.seed(123)
 	summaryTable <- data.frame(
@@ -143,7 +165,7 @@ test_that("table is exported to flextable with total included in a separated row
 	
 })
 
-test_that("row merging for variable group in flextable", {
+test_that("row are merged in case of unique variable group in flextable", {
 			
 	set.seed(123)
 	summaryTable <- data.frame(
@@ -182,7 +204,7 @@ test_that("row merging for variable group in flextable", {
 	
 })
 
-test_that("row merging for statistic in flextable", {
+test_that("rows are merged in case of unique stat in flextable", {
 			
 	set.seed(123)
 	summaryTable <- data.frame(
@@ -224,3 +246,503 @@ test_that("row merging for statistic in flextable", {
 	
 })
 
+test_that("table is exported to flextable with column variables", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		TRT = factor(c("A", "B"), levels = c("B", "A")),
+		n = c(9, 10)
+	)
+	expect_silent(
+		ft <- exportSummaryStatisticsTable(
+			summaryTable = summaryTable,
+			colVar = "TRT"
+		)
+	)
+	expect_s3_class(ft, "flextable")
+	expect_identical(
+		unname(unlist(ft$header$dataset[1, ])),
+		c("B", "A")
+	)
+	
+})
+
+test_that("table is exported to flextable with total in header", {
+	
+	set.seed(123)
+	summaryTable <- data.frame(
+		TRT = factor(c("A", "A", "B", "B"), levels = c("B", "A")),
+		statN = c(1, 4, 2, 5),
+		isTotal = c(FALSE, TRUE, FALSE, TRUE)
+	)
+	
+	# with header:
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				colVar = "TRT",
+				colHeaderTotalInclude = TRUE
+			)
+			unname(unlist(ft$header$dataset))
+		},
+		expected = c("B\n(N=5)", "A\n(N=4)")
+	)
+	
+	# without header
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				colVar = "TRT",
+				colHeaderTotalInclude = FALSE
+			)
+			unname(unlist(ft$header$dataset))
+		},
+		expected = c("B", "A")
+	)
+
+})
+
+test_that("different stat layout when table is exported to flextable", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		variable = c("A", "B"),
+		n = c("1", "2"),
+		Mean = c("0.34", "0.56"),
+		stringsAsFactors = FALSE
+	)
+	
+	# stat in row
+	expect_equal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				rowVar = "variable",
+				statsVar = c("n", "Mean"),
+				statsLayout = "row"
+			)
+			ft$body$dataset
+		},
+		expected = data.frame(
+			c("A", "n", "Mean", "B", "n", 'Mean'),
+			c(NA_character_, "1", "0.34", NA_character_, "2", "0.56"),
+			stringsAsFactors = FALSE
+		),
+		check.attributes = FALSE
+	)
+	
+	# stat in column
+	expect_silent(
+		ft <- exportSummaryStatisticsTable(
+			summaryTable = summaryTable,
+			rowVar = "variable",
+			statsVar = c("n", "Mean"),
+			statsLayout = "col"
+		)		
+	)
+	expect_identical(
+		object = unname(unlist(ft$header$dataset[1, ])),
+		expected = c("variable", "n", "Mean")
+	)
+	expect_equal(
+		object = ft$body$dataset,
+		expected = data.frame(
+			c("A", "B"),
+			c("1", "2"),
+			c("0.34", "0.56"),
+			stringsAsFactors = FALSE
+		),
+		check.attributes = FALSE	
+	)
+	
+	# stat in the row direction, but in a separated column
+	expect_silent(
+		ft <- exportSummaryStatisticsTable(
+			summaryTable = summaryTable,
+			rowVar = "variable",
+			statsVar = c("n", "Mean"),
+			statsLayout = "rowInSepCol"
+		)
+	)
+	expect_identical(unname(unlist(ft$header$dataset[1:2])), c("variable", "Statistic"))
+	expect_equal(
+		object = as.data.frame(sapply(ft$body$dataset, as.character)),
+		expected = data.frame(
+			c("A", "A", "B", "B"),
+			c("n", "Mean", "n", "Mean"),
+			c("1", "0.34", "2", "0.56")
+		),
+		check.attributes = FALSE	
+	)
+	
+})
+
+test_that("stat value label is specified when table is exported to flextable", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		variable = c("A", "B"),
+		Statistic = c("1", "2"),
+		stringsAsFactors = FALSE
+	)
+	expect_match(
+		object = {
+			ft <- exportSummaryStatisticsTable(summaryTable, rowVar = "variable", 
+				statsVar = "Statistic", statsValueLab = "Number of subjects"
+			)
+			unname(unlist(ft$header$dataset[, 2]))
+		},
+		regexp = "Number of subjects.*" # + (N = )
+	)
+			
+})
+
+test_that("stat value label is specified when table is exported to flextable", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		TRT = c("A", "A", "B", "B"),
+		variable = c("a", "b", "a", "b"),
+		n = c("1", "2", "3", "4"),
+		Mean = c("0.1", "1.3", "4.5", "6.7"),
+		stringsAsFactors = FALSE
+	)
+	
+	## only one statistic
+	
+	# include statistical variable name
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				rowVar = "variable", colVar = "TRT",
+				statsVar = "n", 
+				statsLabInclude = TRUE
+			)
+			ft$body$dataset[, 1]
+		},
+		expected = c("a", "n", "b", "n")
+	)
+	
+	# don't include include statistical variable name
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				rowVar = "variable", colVar = "TRT",
+				statsVar = "n", 
+				statsLabInclude = FALSE
+			)
+			ft$body$dataset[, 1]
+		},
+		expected = c("a", "b")
+	)
+	
+	## multiple statistics
+	
+	# the statistical variable name should be included
+	expect_warning(
+		exportSummaryStatisticsTable(
+			summaryTable, 
+			rowVar = "variable", colVar = "TRT",
+			statsVar = c("n", "Mean"),
+			statsLabInclude = FALSE
+		),
+		"Statistic label is included.*because more than one statistic variable.*"
+	)
+	
+})
+
+test_that("a placeholder is specified for empty value when table is exported to flextable", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		TRT = c("A", "A", "B", "B"),
+		PARAM = c("a", "b", "a", "b"),
+		n = c("1", "2", "3", NA_character_),
+		stringsAsFactors = FALSE
+	)
+	
+	# by default, 'empty' value are set to '-'
+	expect_equal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable,
+				rowVar = "PARAM", colVar = "TRT",
+				statsVar = "n"
+			)
+			ft$body$dataset
+		},
+		expected = data.frame(
+			c("a", "b"),
+			c("1", "2"),
+			c("3", "-"),
+			stringsAsFactors = FALSE
+		),
+		check.attributes = FALSE
+	)
+	
+	# custom placeholder for empty value
+	expect_equal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable,
+				rowVar = "PARAM", colVar = "TRT",
+				statsVar = "n",
+				emptyValue = "0"
+			)
+			ft$body$dataset
+		},
+		expected = data.frame(
+			c("a", "b"),
+			c("1", "2"),
+			c("3", "0"),
+			stringsAsFactors = FALSE
+		),
+		check.attributes = FALSE
+	)
+			
+})
+
+test_that("page dimension is specified when the table is exported to flextable", {
+	
+	summaryTable <- data.frame(n = 10)
+	
+	expect_equal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				pageDim = 10,
+				margin = 0
+			)
+			ft$body$colwidths
+		},
+		expected = 10
+	)
+			
+})
+
+test_that("margins are specified when the table is exported to flextable", {
+			
+	summaryTable <- data.frame(n = 10)
+	
+	expect_equal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				pageDim = 10,
+				margin = 1
+			)
+			ft$body$colwidths
+		},
+		expected = 8
+	)
+	
+})
+
+test_that("table is exported to flextable in landscape mode", {
+			
+	summaryTable <- data.frame(n = 10)	
+	
+	# check that width of body in landscape mode > width in portrait mode
+	expect_gt(
+		object = {
+			ftLandscape <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				landscape = TRUE
+			)
+			ftPortrait <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				landscape = FALSE
+			)
+			sum(ftLandscape$body$colwidths) - sum(ftPortrait$body$colwidths)
+		}, 
+		expected = 0
+	)
+	
+})
+
+test_that("title is specified for flextable export", {
+		
+	summaryTable <- data.frame(n = 10)
+	
+	# multiple titles
+	titles <- c("Title A", "Title B")
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				title = titles
+			)
+			ft$header$dataset[seq_along(titles), 1]
+		}, titles
+	)
+			
+})
+
+test_that("footer is specified for flextable export", {
+			
+	summaryTable <- data.frame(n = 10)
+	
+	# multiple footers
+	footers <- c("Explanation 1", "Explanation 2")
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable = summaryTable,
+				footer = footers
+			)
+			ft$footer$dataset[seq_along(footers), 1]
+		}, footers
+	)
+	
+})
+
+test_that("color is specified for flextable export", {
+	
+	set.seed(123)
+	summaryTable <- data.frame(n = 10)
+	
+	colorTable <- c(
+		header = "#330000", headerBackground = "#CC3333",
+		body = "#CCCC00", bodyBackground = "#FFFFCC", 
+		footerBackground = "#CCFFFF", footer = "#000066",
+		line = "#669900"
+	)
+	
+	expect_silent(
+		ft <- exportSummaryStatisticsTable(
+			summaryTable, 
+			colorTable = colorTable,
+			footer = "test"
+		)
+	)
+	# check colors in flextable object
+	expect_setequal(ft$body$styles$text$color$data, colorTable["body"])
+	expect_setequal(ft$body$styles$cells$background.color$data, colorTable["bodyBackground"])
+	expect_setequal(ft$header$styles$text$color$data, colorTable["header"])
+	expect_setequal(ft$header$styles$cells$background.color$data, colorTable["headerBackground"])
+	expect_setequal(ft$footer$styles$text$color$data, colorTable["footer"])
+	expect_setequal(ft$footer$styles$cells$background.color$data, colorTable["footerBackground"])
+	expect_setequal(ft$body$styles$cells$border.color.bottom$data[c(3, 6)], colorTable["line"])
+	
+})
+
+test_that("fontsize is specified for flextable export", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(n = 10)
+	
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				fontsize = 25
+			)
+			c(ft$body$styles$text$font.size$data)
+		}, 
+		expected = 25
+	)
+			
+})
+
+test_that("fontname is specified for flextable export", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(n = 10)
+	
+	expect_identical(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				fontname = "Arial"
+			)
+			c(ft$body$styles$text$font.family$data)
+		}, 
+		expected = "Arial"
+	)
+	
+})
+
+test_that("vertical lines are automatically set in flextable export", {
+				
+	set.seed(123)
+	summaryTable <- data.frame(
+		TRT = c("A", "A", "B", "B"),
+		AVISIT = c("a", "b", "a", "b"),
+		n = c("1", "2", "3", "4"),
+		stringsAsFactors = FALSE
+	)
+	
+	# no vertical lines included
+	expect_setequal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				colVar = c("TRT", "AVISIT"),
+				statsVar = "n",
+				vline = "none"
+			)
+			ft$body$styles$cells$border.width.left$data
+		}, 
+		expected = 0
+	)
+	
+	# vertical line included between subgroups
+	expect_setequal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				colVar = c("TRT", "AVISIT"),
+				statsVar = "n",
+				vline = "auto"
+			)
+			ft$body$styles$cells$border.width.left$data[, c(1, 3)]
+		}, 
+		expected = 1
+	)
+			
+})
+
+test_that("horizontal lines are automatically set in flextable export", {
+			
+	set.seed(123)
+	summaryTable <- data.frame(
+		PARAM = c("a", "b"),
+		n = c("1", "2"),
+		Mean = c("1", "2"),
+		stringsAsFactors = FALSE
+	)
+			
+	# no horizontal lines included
+	expect_setequal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				rowVar = "PARAM",
+				statsVar = c("n", "Mean"),
+				hline = "none"
+			)
+			ft$body$styles$cells$border.width.top$data
+		}, 
+		expected = 0
+	)
+	
+	# horizontal line included between subgroups
+	expect_setequal(
+		object = {
+			ft <- exportSummaryStatisticsTable(
+				summaryTable, 
+				rowVar = "PARAM",
+				statsVar = c("n", "Mean"),
+				hline = "auto"
+			)
+			ft$body$styles$cells$border.width.top$data[4, ]
+		}, 
+		expected = 1
+	)
+	
+})
+			
