1d7634009266dab5dd75d8ce07597659e5f38d77
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-15 18:19:13 +0100

    unit tests: getStats format base stats

--- package/inTextSummaryTable/tests/testthat/test_getStats.R
+++ package/inTextSummaryTable/tests/testthat/test_getStats.R
@@ -1,90 +1,75 @@
 context("Get default set of statistics")
 
-test_that("number of subjects is extracted", {	
+test_that("output format of base stats is correct", {	
+			
+	typeBase <- c("n", "m", "%", "%m", "Mean", "SD", "Median", "SE", "Min", "Max")
+	statBase <- c(n = "statN", `%` = "statPercN", `%m` = "statPercm")
+	statBase <- sapply(typeBase, function(type){
+		if(type %in% names(statBase)){
+			statBase[[type]]
+		}else	paste0("stat", type)
+	})
+	
+	for(type in typeBase){
+				
+		expect_type(getStats(type = !!type), "list")
+		expect_length(getStats(type = !!type), 1)
+		expect_equal(names(getStats(type = !!type)), !!type)
+		expect_type(getStats(type = !!type)[[!!type]], "language")
+				
+		# check if correct stat is specified
+		expect_equal({
+			stat <- getStats(type = !!type)
+			statCode <- grep("stat\\w", as.character(stat[[!!type]]), value = TRUE)
+			sub("(stat\\w)", "\\1", statCode)
+			}, 
+			statBase[[!!type]]
+		)
 		
-	stat <- getStats(type = "n")
-	
-	expect_type(stat, "list")
-	expect_length(stat, 1)
-	expect_named(stat, "n")
-	expect_type(stat$n, "language")
-	
-	# check if correct stat is specified
-	statCode <- grep("stat\\w", as.character(stat$n), value = TRUE)
-	expect_equal(sub("(stat\\w)", "\\1", statCode), "statN")
-
-	summaryTable <- data.frame(statN = c(1, 2, NA_integer_))
-	expect_equal(
-		eval(expr = stat$n, envir = summaryTable),
-		c("1", "2", "NA")
-	)
+	}
 	
 })
 
-test_that("number of records is extracted", {	
+test_that("count/summary base statistics are formatted correctly", {	
 			
-	stat <- getStats(type = "m")
-			
-	expect_type(stat, "list")
-	expect_length(stat, 1)
-	expect_named(stat, "m")
-	expect_type(stat$m, "language")
-	
-	# check if correct stat is specified
-	statCode <- grep("stat\\w", as.character(stat$m), value = TRUE)
-	expect_equal(sub("(stat\\w)", "\\1", statCode), "statm")
-	
-	summaryTable <- data.frame(statm = c(1, 2, NA_integer_))
-	expect_equal(
-		eval(expr = stat$m, envir = summaryTable),
-		c("1", "2", "NA")
-	)
-	
-})
-
-test_that("percentage of subjects is extracted", {	
-			
-	stat <- getStats(type = "%")
-	
-	expect_type(stat, "list")
-	expect_length(stat, 1)
-	expect_named(stat, "%")
-	expect_type(stat$`%`, "language")
-	
-	# check if correct stat is specified
-	statCode <- grep("stat\\w", as.character(stat$`%`), value = TRUE)
-	expect_equal(sub("(stat\\w)", "\\1", statCode), "statPercN")
-	
-	summaryTable <- data.frame(
-		statPercN = c(56.7, 0, 0.999, 1e-6, 99.99, NA_integer_)
-	)
-	expect_equal(
-		eval(expr = stat$`%`, envir = summaryTable),
-		formatPercentage(summaryTable$statPercN)
-	)
-	
-})
-
-test_that("percentage of records is extracted", {	
-			
-	stat <- getStats(type = "%m")
-			
-	expect_type(stat, "list")
-	expect_length(stat, 1)
-	expect_named(stat, "%m")
-	expect_type(stat$`%m`, "language")
-			
-	# check if correct stat is specified
-	statCode <- grep("stat\\w", as.character(stat$`%m`), value = TRUE)
-	expect_equal(sub("(stat\\w)", "\\1", statCode), "statPercm")
+	typeBase <- c("n", "m", "Mean", "SD", "Median", "SE", "Min", "Max")
+	statBase <- c(n = "statN", `%` = "statPercN", `%m` = "statPercm")
+	statBase <- sapply(typeBase, function(type){
+		if(type %in% names(statBase)){
+			statBase[[type]]
+		}else	paste0("stat", type)
+	})
+	for(type in names(statBase)){
 		
-	summaryTable <- data.frame(
-		statPercm = c(56.7, 0, 0.999, 1e-6, 99.99, NA_integer_)
-	)
-	expect_equal(
-		eval(expr = stat$`%m`, envir = summaryTable),
-		formatPercentage(summaryTable$statPercm)
-	)
-	
+		expect_equal({
+				summaryTable <- setNames(
+					data.frame(c(1, 2, NA_integer_)),
+					statBase[!!type]
+				)
+				stat <- getStats(type = !!type)
+				eval(expr = stat[[!!type]], envir = summaryTable)
+			},
+			formatC(x = summaryTable[[statBase[!!type]]])
+		)
+		
+	}
 })
 
+test_that("percentage of subjects/records are formatted correctly", {	
+			
+	statPerc <- c(`%` = "statPercN", `%m` = "statPercm")
+	for(type in names(statPerc)){
+			
+		expect_equal({
+				summaryTable <- setNames(
+					data.frame(c(56.7, 0, 0.999, 1e-6, 99.99, NA_integer_)),
+					statPerc[!!type]
+				)
+				stat <- getStats(type = !!type)
+				eval(expr = stat[[!!type]], envir = summaryTable)
+			},
+			formatPercentage(x = summaryTable[[statPerc[!!type]]])
+		)
+	}
+			
+})
