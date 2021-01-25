013bfe3528f83c1df67ad588efa5cccd236826b2
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-25 17:45:34 +0100

    update vignette and doc

--- package/inTextSummaryTable/R/stats-doc.R
+++ package/inTextSummaryTable/R/stats-doc.R
@@ -2,8 +2,10 @@
 #' 
 #' In the in-text package, different set of statistics are available.
 #' 
-#' The statistics are first computed as numeric,
-#' then formatted to be displayed in the table.
+#' The statistics are first computed as numeric
+#' ('Base statistics' section below),
+#' then formatted to be displayed in the table
+#' ('Formatted statistics' section below).
 #' @template stats-name-base
 #' @template stats-name-formatted
 #' @template stats-formatting
--- package/inTextSummaryTable/man-roxygen/stats-name-base.R
+++ package/inTextSummaryTable/man-roxygen/stats-name-base.R
@@ -1,15 +1,9 @@
 #' @section Base statistics:
 #' 
 #' In the in-text package, the following 'base
-#' statistics' can reported in the output table.\cr
-#' These statistics are reported as numeric and
-#' non rounded in the output table, and are
-#' typically as input for the formatted statistics,
-#' or for plots.
-#' 
-#' Depending on the type of the variable:
+#' statistics' are reported in the summary table:
 #' \itemize{
-#' \item{specific for a continuous variable: }{
+#' \item{for a continuous variable: }{
 #' \itemize{
 #' \item{'statMean': }{variable mean}
 #' \item{'statSD': }{variable standard deviation}
@@ -22,13 +16,23 @@
 #' if multiple and different values are available for a specific variable
 #' and subject ID (by row/column): an error is triggered.
 #' }
-#' \item{for a categorical variable or for a group/full table}{
+#' \item{for a categorical and continuous variable (or the full table): }{
 #' \itemize{
 #' \item{'statN': }{number of subjects}
-#' \item{'statPercN' (or 'statPercm'): }{percentage of subjects
-#' (or records depending on \code{statsPerc})}
 #' \item{'statm': }{number of records}
+#' \item{'statPercN' (or 'statPercm'): }{percentage of subjects
+#' (or records) for the specific group}
+#' \item{'statPercTotalN' (or 'statPercTotalm'): }{number of subjects 
+#' (or records) considered 
+#' for the total (denominator) of the percentage}
+#' }
+#' The percentage and denominator of the percentage are based
+#' on the number of subjects or records depending on
+#' the \code{statsPerc} parameter.
 #' }
 #' }
-#' }
+#' These statistics are reported as numeric and
+#' non rounded in the summary table, and are
+#' typically used as input for the formatted statistics,
+#' or for plots.
 
