aed9bcea34ac8ababd0f22413084884a386975de
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-01-25 15:22:54 +0100

    include startup message and general doc page for pkg

---
+++ package/inTextSummaryTable/R/inTextSummaryTable.R
@@ -0,0 +1,30 @@
+#' inTextSummaryTable: creation of in-text summary table
+#' 
+#' @description Tables of summary statistics or count tables are created. 
+#' These tables can be exported as in-text table to a Clinical Study Report 
+#' (Word format), a topline presentation (PowerPoint format),
+#'  or as interactive table to an html document.
+#'
+#' @details
+#' \itemize{
+#' \item{To get started with the package, see:\cr}{
+#' \code{vignette("inTextSummaryTable-introduction", package = "inTextSummaryTable")}}
+#' \item{To get example code for standard in-text tables created with the package, 
+#' see:\cr}{
+#' \code{vignette("inTextSummaryTable-standardTables", package = "inTextSummaryTable")}}
+#' \item{The main function: \code{\link{getSummaryStatisticsTable}} enables
+#' to create ready in-text table.}
+#' }
+#' @docType package
+#' @name inTextSummaryTable
+NULL
+
+#' @importFrom utils packageVersion
+.onAttach <- function(libname, pkgname) {
+  if (interactive()) {
+    packageStartupMessage(
+        "inTextSummaryTable version ",
+        packageVersion("inTextSummaryTable"),
+        ", see ? inTextSummaryTable to get started")
+  }
+}
