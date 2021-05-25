d6b4181df5cc018d3395b3fd1227f7253a5055ae
lcougnaud <laure.cougnaud@openanalytics.eu>
2021-05-25 13:24:56 +0200

    remove CI/CD spec in testthat.R

--- package/inTextSummaryTable/tests/testthat.R
+++ package/inTextSummaryTable/tests/testthat.R
@@ -1,15 +1,5 @@
 library(testthat)
 library(inTextSummaryTable)
 
-# Note: tests are run in alphabetical order of the scripts in the R folder of the package
-
-if (Sys.getenv("TESTTHAT_OUTPUT_FILE") != "") {
-	options(testthat.output_file = Sys.getenv("TESTTHAT_OUTPUT_FILE", stdout()))
-	#options(VDIFFR_RUN_TESTS = Sys.setenv("VDIFFR_RUN_TESTS" = TRUE))
-}
-test_check(
-	"inTextSummaryTable",
-	reporter = Sys.getenv("TESTTHAT_DEFAULT_CHECK_REPORTER", "check")
-)
-
+test_check("inTextSummaryTable")
 
